a5ab4651aed857e4d5eaeb8a70f701c542cc850d
BigHeadliner <bigheadliner@gmail.com>
2024-02-06 16:57:29 +0200

    getting work started

---
+++ app/js/main.js
@@ -0,0 +1,31 @@
+$(function () {   
+     
+    
+  
+    $('.slider').slick({
+        dots: true, 
+        arrows: false,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+      }); 
+       
+       
+     
+  
+  
+  
+  
+     
+
+ 
+
+
+
+  
+
+
+  
+  
+   
+});
